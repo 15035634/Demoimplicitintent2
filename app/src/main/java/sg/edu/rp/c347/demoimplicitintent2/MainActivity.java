eb1bdbb8ec265cb355ba6dfa556ba77cc990e3bb
15035634 <15035634@myrp.edu.sg>
2017-05-04 12:27:04 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c347/demoimplicitintent2/MainActivity.java
@@ -0,0 +1,31 @@
+package sg.edu.rp.c347.demoimplicitintent2;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnRP;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnRP = (Button) findViewById(R.id.buttonRP);
+        btnRP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent rpIntent = new Intent(Intent.ACTION_VIEW);
+
+                rpIntent.setData(Uri.parse("http://www.rp.edu.sg"));
+                startActivity(rpIntent);
+            }
+        });
+
+    }
+}
